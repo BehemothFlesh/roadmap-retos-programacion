b54ba4c0ddf44f931974e9f614e85c20f21a0837
BehemothFlesh <162525717+BehemothFlesh@users.noreply.github.com>
2024-03-06 20:50:41 -0600

    Create BehemothFlesh.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BehemothFlesh.py
@@ -0,0 +1,27 @@
+#https://.python.org
+
+#comentarios en una linea
+
+"""
+Asi se programa en
+varias 
+lineas
+"""
+'''
+asi tambien se puede
+'''
+
+my_variable = "mi variable"
+my_variable = "valor de mi variable"
+
+# phyton no tiene constantes
+# Datos primitivos de phyton
+my_int = 23
+my_float = 14.76
+my_bool = True
+my_bool = False
+my_string = "pepinillo"
+my_other_string = 'pepinillo dos'
+
+print("hola phayton")
+print("hola rubi teamo")
